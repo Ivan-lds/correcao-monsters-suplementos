bae13b3fefa1269789c293449fb39fc14fba815a
Ivan Lima <ivanlimadossantos4@gmail.com>
2024-10-09 15:45:11 -0300

    Add files via upload

---
+++ biblioteca.js
@@ -0,0 +1,153 @@
+let dados = [
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaJTfH7PW1DhWVD4ECX6YtEDwrby900un-TA&s",
+    produto: "Creatina 300g - Probiótica Monohidratada",
+    preco: "R$ 97,40",
+  },
+  {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3PiPB486MRdlx2rz5gOuJMH511wMphDZadQ&s",
+    produto:
+      "Combo 100% Pure Whey + Creatina 300g Probiótica + Coqueteleira World",
+    preco: "R$ 199,90",
+  },
+  {
+    img: "",
+    produto: "Creatina Monohidratada 120 Caps - New Millen Sabor Sem Sabor",
+    preco: "R$ 60,42",
+  },
+  {
+    img: "https://a-static.mlcdn.com.br/450x450/creatina-pura-100-monohidratada-250g-vitamina-d-2000ui-em-gotas20ml-denavita/lojadominshop/e44e548ef5b911edabc64201ac185049/8f66ac3a9cf4c032e9ab58d93a72b843.jpeg",
+    produto:
+      "Creatina Pura 100% Monohidratada 250g + Vitamina D 2000UI em Gotas20ml - Denavita",
+    preco: "R$ 99,90",
+  },
+  {
+    img: "https://images.tcdn.com.br/img/img_prod/724458/creatina_300_g_max_titanium_1391_1_b05ded374af15ee807ffe1fbaeb99059.jpg",
+    produto:
+      "Max Titanium Creatina - Suplemento alimentar de creatina em pó, 300g",
+    preco: "R$ 66,69",
+  },
+  {
+    img: "https://cdn.awsli.com.br/300x300/1190/1190852/produto/120851731/71061f8123.jpg",
+    produto: "MASS TITANIUM 17500 3KG - MAX TITANIUM",
+    preco: "R$ 99,90",
+  },
+  {
+    img: "https://www.drshape.com.br/media/catalog/product/cache/2/thumbnail/9df78eab33525d08d6e5fb8d27136e95/c/r/creatina_1_1.jpg?v=1",
+    produto: "Creatina 300gr - Dr. Shape",
+    preco: "R$ 149,00",
+  },
+  {
+    img: "https://integralmedica.vtexassets.com/arquivos/ids/166583-800-auto?v=638484414698670000&width=800&height=auto&aspect=true",
+    produto: "Creatine Darkness 300g",
+    preco: "R$ 111,92",
+  },
+  {
+    img: "https://acdn.mitiendanube.com/stores/001/354/536/products/1_1401470_469432471-d2cc1b4e790681528c16997478371852-640-0.jpeg",
+    produto: "Creatine Monohydrate 300g - Universal Nutrition",
+    preco: "R$ 97,99",
+  },
+  {
+    img: "https://www.jacaresuplementos.com/media/product/8c8/creatina-powder-max-300g-pro-size-nutrition-pro-size-nutrition-c40.jpg",
+    produto: "Creatina Powder Max (300g) - Pro Size Nutrition",
+    preco: "R$ 107,45",
+  },
+  {
+    img: "https://m.media-amazon.com/images/I/41Fx8sEhd9L._SR600%2C315_PIWhiteStrip%2CBottomLeft%2C0%2C35_SCLZZZZZZZ_FMpng_BG255%2C255%2C255.jpg",
+    produto: "Whey Protein 1,820 Kg - Masterway - Sabor Chocolate/Baunilha",
+    preco: "R$ 240,10",
+  },
+  {
+    img: "https://down-br.img.susercontent.com/file/72040cfc9155b0ec8bb76fc2ec3173cd",
+    produto:
+      "Whey Protein 90% Isolado 1kg Original - Growth Supplements (Chocolate)",
+    preco: "R$ 174,10",
+  },
+
+  {
+    img: "https://www.masterwaysuplementos.com.br/image/cachewebp/catalog/fotos-seo/whey-protein-isolado-910g-sabor-chocolate-masterway-suplementos-200x200.webp",
+    produto: "Masterway Suplementos - Whey Protein Isolado - 910g (Chocolate)",
+    preco: "R$ 149,90",
+  },
+  {
+    img: "https://cdn.awsli.com.br/300x300/395/395149/produto/239928541/nutri-whey-pote-17mdgiu9sy.jpg",
+    produto: "Nutri Whey Protein 907g Integralmedica",
+    preco: "R$ 82,94",
+  },
+  {
+    img: "https://cdn.awsli.com.br/300x300/2404/2404599/produto/252433497742a0e8fb1.jpg",
+    produto: "Whey Protein 100% Dark Lab Chocolate 900g - Dark Lab",
+    preco: "R$ 126,90",
+  },
+  {
+    img: "https://www.gsuplementos.com.br/upload/produto/imagem/top-whey-protein-concentrado-1kg-growth-supplements-19.webp",
+    produto: "Whey Protein Growth, Proteína do Soro do Leite Pura",
+    preco: "R$ 108,00",
+  },
+  {
+    img: "https://lojamaxtitanium.vtexassets.com/arquivos/ids/157350/100-whey-protein-max-titanium-dr-peanut-900g-avela-1.jpg?v=638343752603430000",
+    produto: "100% Whey Max Titanium x Dr. Peanut (900g), Sabor Avelã",
+    preco: "R$ 130,32",
+  },
+  {
+    img: "https://images.tcdn.com.br/img/img_prod/677409/100_whey_protein_leite_900g_1967_1_b859418befbc2b23208d5af8bdde17db.png",
+    produto: "100% Whey protein- chocolate -900gFTW",
+    preco: "R$ 121,60",
+  },
+  {
+    img: "https://d8vlg9z1oftyc.cloudfront.net/ailos/image/product/45fb901af2fea66919896d219f177a4920220707155848/original/100-whey-gold-standard-900g-baunilha-optimum-nutrition_2971.png",
+    produto: "Optimum Nutrition Gold Standard 100% Whey protein Powder",
+    preco: "R$ 669,50",
+  },
+  {
+    img: "https://images.tcdn.com.br/img/img_prod/748920/whey_protein_concentrado_refil_1kg_pro_healthy_127_1_8aaca28150ceedd332f59dcfdfc968b2.jpg",
+    produto: "Whey Protein Concentrado -Refil 1Kg- Pro Healthy",
+    preco: "R$ 44,99",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+  {
+    img: "",
+    produto: "",
+    preco: "",
+  },
+];
